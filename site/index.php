cfb03dd1cc31b44a3aae7e739ad2fbf0a93d1ffe
NolanMlb <77735812+NolanMlb@users.noreply.github.com>
2021-11-22 15:48:42 +0100

    index css verif

--- site/index.php
+++ site/index.php
@@ -13,24 +13,34 @@
     </head> 
 
     <body>
-        <h1>CONNECTEZ VOUS POUR ACCEDER A VOTRE ESPACE </h1>
+        <h1 class="titre">CONNECTEZ-VOUS POUR ACCEDER A VOTRE ESPACE </h1>
 
 <!-- Formulaire de connexion -->
         <form action="verification.php" method="POST">
-            <label><b>Nom d'utilisateur :</b></label>
-            <input type="text" placeholder="Entrez le nom d'utilisateur" name="username" requiered>
+        <div class="form-group">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="utilisateur" placeholder="Entrez le nom d'utilisateur..." name="username" requiered>
+                    <label for="floatingInput">Nom d'utilisateur</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe..." name="password" requiered>
+                    <label for="floatingPassword">Mot de passe</label>
+                </div>
+        </div>
 
-            <label><b>Mot de passe :</b></label>
-            <input type="password" placeholder="Entrez le mot de passe" name="password" requiered>
 
-            <?php
+            <button type="submit" class="btn btn-primary">Valider</button>
+
+        <?php
                 if(isset($_POST['erreur'])){
                     $err = $_POST['erreur'];
                     if($err == 1 || $err == 2){
                         echo "<p style='color:red'>Utilisateur ou mot de passe incorrect </p>";
                     }
                 }
-            ?>
+        ?>
+
+
         </form>
             
     </body>
