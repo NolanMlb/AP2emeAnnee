b2a3ec387d92ef82209db145ea0aa9be5ced2340
zairoxxxxx <77746469+zairoxxxxx@users.noreply.github.com>
2022-01-06 11:58:52 +0100

    changement sur l'accueil, entete et finition sur affecter visite

--- site/controleur/affecterVisite.php
+++ site/controleur/affecterVisite.php
@@ -1,21 +1,26 @@
 <?php
 
     //include les fichiers
-    include "$racine/modele/bd.inc.php";
-    include "$racine/controleur/connexion.php";
-
+    include "../modele/bd.inc.php";
+    include "../controleur/connexion.php";
+    include "../vue/entete.html";
+    
+    //recuperer l'id
+    $id_auto=0;
 
     //interprétation pour la base de donnée
-    $mysqli = new mysqli("localhost", "root", "root", "ap2eme");
+    $mysqli = new mysqli("localhost:3306", "root", "", "ap2eme");
     $mysqli -> set_charset("utf-8");
-    $requete="INSERT INTO intervention (id)VALUES ('','$_POST[dateVisite]','$_POST[heureVisite]','$_POST[idClient]','$_POST[idTechnicien]')";
-    
+    $requete="INSERT INTO intervention VALUES ($id_auto,'$_POST[dateVisite]','$_POST[heureVisite]','$_POST[idClient]','$_POST[idTechnicien]')";
     $resultat = mysqli_query($mysqli,$requete);
+
+    
+    // test si la requete est bien faite
     if ($resultat){
-        echo"Requete a été ajouté";
+        echo"Le rendez-vous a bien été ajouté <br>";
     }
     else{
-        echo "non";
+        echo "Erreur, je n'ai pas reussie à ajouté le rdv !<br> ";
     }
 ?>
-<a href="vue/accueil.html">Retour a la page d'accueil</a>
+<a href="../vue/accueil.html">Retour a la page d'accueil</a>
