af72038718e8ab2abb6fd56599385fcc97021bca
zairoxxxxx <77746469+zairoxxxxx@users.noreply.github.com>
2022-01-31 14:18:35 +0100

    java

---
+++ Jalon 3/src/main/java/Ap2emeAnnee/Famille.java
@@ -0,0 +1,13 @@
+package Ap2emeAnnee;
+
+public class Famille {
+
+    //attributs
+    private String codeFamille, libelleFamille;
+
+    //constructeur
+    public Famille(String codeFamille, String libelleFamille) {
+        this.codeFamille = codeFamille;
+        this.libelleFamille = libelleFamille;
+    }
+}
---
+++ Jalon 3/src/main/java/Ap2emeAnnee/PersistanceSQL.java
@@ -0,0 +1,10 @@
+package Ap2emeAnnee;
+
+import java.util.*;
+
+public class PersistanceSQL {
+
+    private String ipBase, port, nomBaseDonnee;
+
+
+}
