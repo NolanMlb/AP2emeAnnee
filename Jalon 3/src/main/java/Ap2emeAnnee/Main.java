b1c24ee4ebbb72261d7b8e31b746e5dfd28bb326
niquolic <nicolas.debruycker22@gmail.com>
2022-05-06 11:00:47 +0200

    aaa

--- Jalon 3/src/main/java/Ap2emeAnnee/Main.java
+++ Jalon 3/src/main/java/Ap2emeAnnee/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ParametresBDD unParametre = new ParametresBDD("root", "", "ap2eme",
-                "jdbc:mysql:///");
+        ParametresBDD unParametre = new ParametresBDD("root", "root", "ap2eme",
+                "jdbc:mysql:///localhost:8888");
 
         Connection connexion = null;
         try {
